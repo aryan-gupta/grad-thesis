8ee0dc0489f7120c562718b1dd24e4a778cf8fb2
Aryan <me@theguptaempire.net>
2022-06-21 15:48:31 -0400

    Move more img processing functions into img_process file

--- risk-aware-planning/code/pathfind_image.py
+++ risk-aware-planning/code/pathfind_image.py
@@ -38,83 +38,12 @@ wpcc_img = img_process.perspective_warp(img, points, map_w, map_h)
 
 (red_channel, green_channel, blue_channel, yellow_channel) = img_process.color_segment_image(wpcc_img)
 
-##################################### Reward Image Construction ##############################################
+processed_img = img_process.merge_colors(red_channel, green_channel, blue_channel, yellow_channel)
 
-
-reward_size = 128
-dilate_kernel = np.ones((reward_size,reward_size), np.uint8)
-gaussian_kernel_size = reward_size + 1
 orig_goal_reward_image = cv2.add(cv2.add(red_channel, blue_channel), yellow_channel)
+goal_reward_image = img_process.apply_edge_blur(orig_goal_reward_image, 128)
 
-def apply_edge_blur(orig):
-    contours, hierarchy = cv2.findContours(orig, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    new_img = np.zeros((map_h, map_w, 1), dtype = "uint8")
-
-    num_cnt = len(contours)
-    for cnts in contours:
-        mask = np.zeros((map_h, map_w, 1), dtype = "uint8")
-        # (x,y),radius = cv2.minEnclosingCircle(cnts)
-        # center = (int(x),int(y))
-        # radius = int((reward_size/2) + math.sqrt(2) * radius)
-        # print(radius)
-        # cv2.circle(mask,center,radius,(255),-1)
-        mask = cv2.drawContours(mask, [cnts], -1, (255), -1)
-        mask = cv2.dilate(mask, dilate_kernel, 0)
-        # plt.imshow(mask, cmap='gray'); plt.show()
-        # mask = cv2.bilateralFilter(mask, reward_size*16, 1000, 1000)
-        # mask = cv2.blur(mask, (gaussian_kernel_size, gaussian_kernel_size))
-        mask = cv2.GaussianBlur(mask, (gaussian_kernel_size, gaussian_kernel_size), reward_size)
-        new_img = cv2.scaleAdd(mask, (1/num_cnt), new_img)
-        # plt.imshow(new_img, cmap='gray'); plt.show()
-    
-    return cv2.normalize(new_img, None, 255, 0, norm_type = cv2.NORM_MINMAX)
-    
-# plt.imshow(goal_reward_image, cmap='gray'); plt.show()
-goal_reward_image = apply_edge_blur(orig_goal_reward_image)
-# plt.imshow(goal_reward_image, cmap='gray'); plt.show()
-
-# goal_reward_image = cv2.bitwise_or(goal_reward_image, orig_goal_reward_image)
-
-# To do a guassian distribution around the edges, we first dialate the mask the same amount
-# as much as we want to do the gaussian blur 
-
-##################################### Risk Image Construction ##############################################
-
-# Wall risk image
-risk_size = 16
-dilate_kernel = np.ones((risk_size,risk_size), np.uint8)
-gaussian_kernel_size = risk_size + 1
-wall_risk_image = cv2.dilate(green_channel, dilate_kernel, 0)
-wall_risk_image = cv2.GaussianBlur(wall_risk_image, (gaussian_kernel_size, gaussian_kernel_size), 0)
-wall_risk_image = cv2.bitwise_or(wall_risk_image, green_channel)
-# plt.imshow(wall_risk_image, cmap='gray'); plt.show()
-
-risk_image = wall_risk_image
-# purple_channel = risk_image
-
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-# xx, yy = np.mgrid[0:risk_image.shape[0], 0:risk_image.shape[1]]
-# ax.plot_surface(xx, yy, risk_image, rstride=1, cstride=1, linewidth=0)
-# plt.show()
-
-##################################### Final Image Construction ##############################################
-
-# We want to convert the different color channels into an RGB image and since yellow is Red and Green
-# we want add the yellow channel into the red and green channels 
-red_channel = cv2.bitwise_or(red_channel, yellow_channel)
-green_channel = cv2.bitwise_or(green_channel, yellow_channel)
-
-# red_channel = cv2.bitwise_or(red_channel, purple_channel)
-# blue_channel = cv2.bitwise_or(blue_channel, purple_channel)
-
-# plt.imshow(red_channel, cmap='gray'); plt.show()
-# plt.imshow(green_channel, cmap='gray'); plt.show()
-
-# Merge the channels into one RGB image
-processed_img = cv2.merge([red_channel,green_channel,blue_channel])
-# plt.imshow(processed_img); plt.show()
-
+risk_image = img_process.create_risk_img(green_channel, 16)
 
 ##################################### Basic Image Manip ##############################################
 
