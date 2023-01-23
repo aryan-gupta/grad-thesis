a79d14fd6c5df5d6b9b37d55d0be2476a629b526
Aryan Gupta <me@theguptaempire.net>
2023-01-22 20:32:29 -0500

    Update test

--- tests/self-update-test.py
+++ tests/self-update-test.py
@@ -3,29 +3,42 @@
 
 class A:
     def __init__(self):
+        self.state = 'A'
         pass
+
+    def pas(self):
+        print(self.state)
 
     def pa(self):
         print("helloA")
 
     def update(self):
-        u = B()
-        # self = u
-        return u
+        self.__class__ = B
+        self.__init__(self)
 
 
 class B(A):
-    def __init__(self):
+    def __init__(self, a):
+        self.state = 'B'
         pass
 
     def pb(self):
         print("helloB")
 
+    def pabs(self):
+        print(self.state)
+
+    # def promote(self):
+    #     # self.state = 'B'
+    #     pass
+
 
 
 a = A()
 a.pa()
-# a.update()
-a = a.update()
+a.pas()
+a.update()
+# a = a.update()
 a.pb()
 a.pa()
+a.pabs()
