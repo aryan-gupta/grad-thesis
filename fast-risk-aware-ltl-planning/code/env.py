aryan-gupta/grad-thesis

commit 4e348929d5f8082d0703668a766a1653f68657b0

[Untested] Remove envTMP in pathfinder code

fast-risk-aware-ltl-planning/code/env.py
@@ -144,6 +144,13 @@ class EnviromentCreator:
         cv2.imwrite(filename, env_bgr)
 
 
+# this class is a place holder to store an minimal environment
+# this will be deprecated in the future, hopefully
+class EnviromentMinimal:
+    def __init__(self):
+        self.cell_cost = None
+        self.cell_type = None
+
 
 class Enviroment(EnviromentCreator):
     def __init__(self, filename=None):
@@ -214,6 +221,11 @@ class Enviroment(EnviromentCreator):
 
         cv2.imwrite(filename, cv2.cvtColor(dj_path_image, cv2.COLOR_RGB2BGR))
 
+    @staticmethod
+    def get_minimal_env(cell_type, cell_cost):
+        envMin = EnviromentMinimal()
+        envMin.cell_type = cell_type
+        envMin.cell_cost = cell_cost
 
 def main():
     random.seed(0)

fast-risk-aware-ltl-planning/code/pathfinder.py
@@ -16,12 +16,6 @@ import env
 import random
 import optimizer
 
-class EnvTMP:
-    def __init__(self):
-        self.cell_cost = None
-        self.cell_type = None
-
-
 
 class OutputType(Enum):
     NONE       = 0 # do not output anything
@@ -122,10 +116,7 @@ class Pathfinder:
                 # print(final_phys_loc)
 
                 # apply dj's algo
-                envTMP = EnvTMP()
-                envTMP.cell_type = risk_reward_cell_type_local
-                envTMP.cell_cost = risk_reward_cell_cost_local
-                opt = optimizer.Optimizer(envTMP, self.task)
+                opt = optimizer.Optimizer(env.Enviroment.get_minimal_env(risk_reward_cell_type_local, risk_reward_cell_cost_local), self.task)
                 opt.set_task_state(0, self.current_ltl_state)
                 current_planned_path = dijkstra.astar_opt(risk_reward_img_cells_local, risk_reward_cell_type_local, (self.current_phys_loc, final_phys_loc), risk_reward_cell_cost_local, main.CELLS_SIZE, opt)
                 if show: print(len(current_planned_path))
@@ -139,10 +130,7 @@ class Pathfinder:
 
                 if amount_risk_updated > 0:
                     if show: print("full astar replanning")
-                    envTMP = EnvTMP()
-                    envTMP.cell_type = risk_reward_cell_type_local
-                    envTMP.cell_cost = risk_reward_cell_cost_local
-                    opt = optimizer.Optimizer(envTMP, self.task)
+                    opt = optimizer.Optimizer(env.Enviroment.get_minimal_env(risk_reward_cell_type_local, risk_reward_cell_cost_local), self.task)
                     opt.set_task_state(0, self.current_ltl_state)
                     current_planned_path = dijkstra.astar_algo(risk_reward_img_cells_local, risk_reward_cell_type_local, (self.current_phys_loc, final_phys_loc), risk_reward_cell_cost_local, main.CELLS_SIZE)
                 elif amount_risk_updated > 0:

commit b3ae12509c70a65fcd0bcfa1d41e73486c74121c

Fix bug introduced by 4e348929d5f8082d0703668a766a1653f68657b0
- The envMin was not returned so optimizer constructer got a null value
  for the environment

fast-risk-aware-ltl-planning/code/env.py
@@ -230,6 +230,8 @@ class Enviroment(EnviromentCreator):
         envMin.cell_type = cell_type
         envMin.cell_cost = cell_cost
 
+        return envMin
+
 def main2():
     random.seed(0)
     e = EnviromentCreator(targets=4, size=(800,800), validate=False)